f3089c55e1edc11e0cc434d2e8b192a66e254856
Joy <travelerjoy85@gmail.com>
2017-04-04 22:26:02 +0000

    capitalization done

--- workshop.js
+++ workshop.js
@@ -104,7 +104,11 @@ function longestWord(inputString) {
 }
 
 function capitalize(inputString) {
-
+  var str = inputString.toLowerCase().split(" ");
+  for(var i = 0; i < str.length; i++){
+      str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+  }
+  return str.join(" ");
 }
 
 function sumOfNumbers(arrayOfNumbers) {
